f2050e4c8f24c3e47537645c80933659a17655a1
ppiserchia <piserchia.pasquale@outlook.it>
2024-12-04 14:58:44 +0100

    Initial Commit

---
+++ DyslexiAid/DyslexiAidApp.swift
@@ -0,0 +1,17 @@
+//
+//  DyslexiAidApp.swift
+//  DyslexiAid
+//
+//  Created by Pasquale Piserchia on 04/12/24.
+//
+
+import SwiftUI
+
+@main
+struct DyslexiAidApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
